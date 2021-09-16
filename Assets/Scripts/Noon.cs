85e15d96b41debd6d39655b9df9b7fd42b45e319
Durrell Miree <dmiree@ltu.edu>
2021-09-16 16:32:48 -0400

    Initial commit

---
+++ Assets/Scripts/Noon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noon : MonoBehaviour
+{
+   //Take B - Rotating around an axis
+    public float rotationSpeed = 1;
+    public float rotationLength = 10;
+    public float sAngle;
+    public float eAngle;
+    float curVel = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Take A - SmoothDamping between two vectors 
+        //this.transform.eulerAngles = startAngle;
+
+        Vector3 angle = this.transform.eulerAngles;
+        angle.z = sAngle;
+        this.transform.eulerAngles = angle; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Take A - SmoothDamping between two vectors 
+        //this.transform.eulerAngles = Vector3.SmoothDamp(this.transform.eulerAngles, endAngle, ref curVel, rotationSpeed);
+
+        //Take B - Rotating around an axis. Worked as intended
+        Vector3 tempAngle = this.transform.eulerAngles;
+        tempAngle.z = tempAngle.z + rotationSpeed * Time.deltaTime;
+        //tempAngle.z += rotationSpeed; 
+
+        this.transform.eulerAngles = tempAngle; 
+    }
+}
---
+++ Assets/Scripts/Wonder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wonder : MonoBehaviour
+{
+   float Speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temP = this.gameObject.transform.position;
+
+        //temP.x = temP.x + Speed; 
+        temP.x = temP.x + Speed;
+
+        //float val = 0;
+
+        //val = val + Speed;
+
+        this.gameObject.transform.position = temP;
+    }
+}
